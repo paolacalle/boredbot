0735517f6c90b8ce5bfc0870ff9a9c667939bd24
Paola Calle <pcalle@smith.edu>
2023-06-27 21:05:58 +0000

    Added a database to our project, created our style checker as well

--- bored.py
+++ bored.py
@@ -1,15 +1,17 @@
 import requests
 import pprint 
+import pandas as pd
+import sqlalchemy as db
 
 baseURL = "http://www.boredapi.com/api/activity?"
-attributes = ['Type', 'Price Range', 'Accessibility', 'Participants']
+attributes = ['type', 'price Range', 'accessibility', 'participants']
 
 def get_type():
     types = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
     print("\nChoose one of the following types: ")
     for item in types:
         print(item)
-    selectedType = input()
+    selectedType = input().lower()
 
     if selectedType in types:
         typeURL = "type=" + selectedType
@@ -20,10 +22,10 @@ def get_price_range():
     priceRange = "(0.00 being free and 1.00 being the most expensive)" 
 
     print("\nWhat is your minimum price range for an activity?" + priceRange)
-    selectedMin = input()
+    selectedMin = input().lower()
 
     print("\nWhat is your maximum price range for an activity?" + priceRange)
-    selectedMax = input()
+    selectedMax = input().lower()
 
     priceURL = "minprice=" + selectedMin + "&maxprice=" + selectedMax
     get_activity(priceURL)
@@ -32,7 +34,7 @@ def get_accessibility():
     accesibilityRating = "(0.0 being the least accessible and 1.0 being the most accessible)"
 
     print("\nWhat is your desired accesibility rating for an activity? " + accesibilityRating)
-    selectedAR = input()
+    selectedAR = input().lower()
 
     accessbilityURL = "accessibility=" + selectedAR
     get_activity(accessbilityURL)
@@ -41,7 +43,7 @@ def get_participants():
     participantRange = "(You can choose between 1-8 participants)"
 
     print("\nWhat is your desired number of participants? " +participantRange)
-    selectedParticipantNum = input()
+    selectedParticipantNum = input().lower()
 
     participantURL = "participants=" + selectedParticipantNum
     get_activity(participantURL)
@@ -55,33 +57,41 @@ def get_activity(url):
         display_response(message)
 
 def get_opinion():
-    print("\nDo you like this activity or would you like another? (Yes or No)")
-    likeResponse = input()
-    if likeResponse == "Yes":
-        return True
-    elif likeResponse == "No":
-        return False 
+    print("\nWould you like another? (Yes or No)")
+    likeResponse = input().lower()
 
-#build print def 
+    if likeResponse not in ["yes", "no"]:
+        get_opinion()
+
+    if likeResponse == "yes":
+        return False
+    else:
+        return True 
+
+
+#Displaying Dataframe 
 def display_response(message):
-    print(f"\nActivity: {message['activity']}")
-    print(f"Type: {message['type']}")
-    print(f"Accessibility: {message['accessibility']}")
-    print(f"Participants: {message['participants']}")
-    print(f"Price: {message['price']}")   
+    df = pd.DataFrame.from_dict(message, orient= 'index')
+    pprint.pprint(df)
+
+    # print(f"\nActivity: {message['activity']}")
+    # print(f"Type: {message['type']}")
+    # print(f"Accessibility: {message['accessibility']}")
+    # print(f"Participants: {message['participants']}")
+    # print(f"Price: {message['price']}")   
     
 def call_attribute(attr): 
-    if attr == "Type": 
+    if attr == "type": 
         get_type()
-    elif attr == "Price Range": 
+    elif attr == "price range": 
         get_price_range()
-    elif attr == "Accessibility":
+    elif attr == "accessibility":
         get_accessibility()
-    else:
+    elif attr == "participants":
         get_participants()
 
 print(f"\nWhat attributes do you care about the most? {attributes}.")
-attr = input()
+attr = input().lower()
 end = False 
 
 while not end: 
@@ -91,18 +101,18 @@ while not end:
         end = get_opinion()
     
         if end == False:
-            print("d\nould you like to stay in this same category or switch to a different one? (Stay or Switch)")
-            categoryResponse = input()               
+            print("\n\nWould you like to stay in this same category or switch to a different one? (Stay or Switch)")
+            categoryResponse = input().lower()               
             if categoryResponse == "Stay":
                 call_attribute(attr) 
                 
             else:
                 print(f"\nWhat attributes do you care about the most? {attributes}")
-                attr = input()
+                attr = input().lower()
                 call_attribute(attr)
 
 
     else:
         print("\nInput a valid attribute: (Type, Price Range, Accessibility, Participants)")
-        attr = input()
+        attr = input().lower()
         call_attribute(attr)
