0393018a0f333f027b9a2fb8fe0eea2cd45bc01f
Paola Calle <pcalle@smith.edu>
2023-06-29 20:35:30 +0000

    Style bored

--- bored.py
+++ bored.py
@@ -1,3 +1,156 @@
+import unittest
+import requests
+from unittest.mock import patch
+from io import StringIO
+import sys
+from bored import get_type, get_price_range, get_accessibility, get_participants, get_opinion, stay_switch
+
+
+class unit_test(unittest.TestCase):
+
+    @patch('builtins.input', side_effect=['education'])
+    def test_get_type_vaild_input(self, mock_input):
+        expected = 'type=education'
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = get_type()
+
+        self.assertEqual(result, expected)
+        mock_input.assert_called_once_with()
+
+    @patch('builtins.input', side_effect=['invalid', 'education'])
+    def test_get_type_invalid_input_then_valid_input(self, mock_input):
+        expected = 'type=education'
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = get_type()
+            output = mock_stdout.getvalue().strip()
+
+        self.assertEqual(result, expected)
+        self.assertEqual(mock_input.call_count, 2)
+        mock_input.assert_has_calls(
+            [unittest.mock.call(), unittest.mock.call()])
+
+    @patch('builtins.input', side_effect=['0', '1'])
+    def test_get_price_range_vaild_input(self, mock_input):
+        expected = 'minprice=0&maxprice=1'
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = get_price_range()
+
+        self.assertEqual(result, expected)
+        self.assertEqual(mock_input.call_count, 2)
+        mock_input.assert_has_calls(
+            [unittest.mock.call(), unittest.mock.call()])
+
+    @patch('builtins.input', side_effect=['100', '0', '-1', '1'])
+    def test_get_price_range_invaild_then_vaild_input(self, mock_input):
+        expected = 'minprice=0&maxprice=1'
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = get_price_range()
+            output = mock_stdout.getvalue().strip()
+
+        self.assertEqual(result, expected)
+        self.assertEqual(mock_input.call_count, 4)
+        mock_input.assert_has_calls([unittest.mock.call(
+        ), unittest.mock.call(), unittest.mock.call(), unittest.mock.call()])
+
+    @patch('builtins.input', side_effect=['1'])
+    def test_get_accessibility_valid_input(self, mock_input):
+        expected = 'accessibility=1'
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = get_accessibility()
+
+        self.assertEqual(result, expected)
+        mock_input.assert_called_once_with()
+
+    @patch('builtins.input', side_effect=['2', '0'])
+    def test_get_accessibility_invalid_then_valid_input(self, mock_input):
+        expected = 'accessibility=0'
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = get_accessibility()
+
+        self.assertEqual(result, expected)
+        self.assertEqual(mock_input.call_count, 2)
+        mock_input.assert_has_calls(
+            [unittest.mock.call(), unittest.mock.call()])
+
+    @patch('builtins.input', side_effect=['2'])
+    def test_get_participants_valid_input(self, mock_input):
+        expected = 'participants=2'
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = get_participants()
+
+        self.assertEqual(result, expected)
+        mock_input.assert_called_once_with()
+
+    @patch('builtins.input', side_effect=['0', '1'])
+    def test_get_participants_invalid_then_valid_input(self, mock_input):
+        expected = 'participants=1'
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = get_participants()
+
+        self.assertEqual(result, expected)
+        self.assertEqual(mock_input.call_count, 2)
+        mock_input.assert_has_calls(
+            [unittest.mock.call(), unittest.mock.call()])
+
+    @patch('builtins.input', side_effect=['yes'])
+    def test_get_opinion_valid_input(self, mock_input):
+        expected = False
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = get_opinion()
+
+        self.assertEqual(result, expected)
+        mock_input.assert_called_once_with()
+
+    @patch('builtins.input', side_effect=['silly', 'no'])
+    def test_get_opinion_invalid_then_valid_input(self, mock_input):
+        expected = True
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = get_opinion()
+
+        self.assertEqual(result, expected)
+        self.assertEqual(mock_input.call_count, 2)
+        mock_input.assert_has_calls(
+            [unittest.mock.call(), unittest.mock.call()])
+
+    @patch('builtins.input', side_effect=['switch'])
+    def test_stay_switch_valid(self, mock_input):
+        expected = True
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = stay_switch()
+
+        self.assertEqual(result, expected)
+        mock_input.assert_called_once_with()
+
+    @patch('builtins.input', side_effect=['BREAK', 'stay'])
+    def test_stay_switch_invalid_then_valid_input(self, mock_input):
+        expected = False
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            result = stay_switch()
+
+        self.assertEqual(result, expected)
+        self.assertEqual(mock_input.call_count, 2)
+        mock_input.assert_has_calls(
+            [unittest.mock.call(), unittest.mock.call()])
+
+
+if __name__ == '__main__':
+    unittest.main()
+codio@heliumalien-fiestaalex:~/workspace/bored_bot$ autopep8 -v bored.py
+[file:bored.py]
+--->  9 issue(s) to fix {'E128': {40, 41, 42, 43, 44, 45, 46, 47}, 'W291': {118}}
+--->  0 issue(s) to fix {}
 import requests
 import pprint
 import pandas as pd
@@ -36,7 +189,15 @@ def save_activity(activity):
 
 
 def get_type():
-    types = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+    types = ["education",
+             "recreational",
+             "social",
+             "diy",
+             "charity",
+             "cooking",
+             "relaxation",
+             "music",
+             "busywork"]
     foundType = False
     while foundType is False:
         print("\nChoose one of the following types:\n")
@@ -107,7 +268,7 @@ def get_participants():
 
         if selectedParticipantNum <= 0:
             print("\nYou need at least one participant.")
-        else: 
+        else:
             participantURL = f"participants={selectedParticipantNum}"
             return participantURL
 
